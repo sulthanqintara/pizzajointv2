4a9472a52e4cc29520f212fd3dd9d0ea830ca8d8
Sulthan Qintara <sulthanqintara@gmail.com>
2023-09-13 21:17:26 +0700

    Update dependencies, add animations, transitions.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,28 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
-        Welcome to Pizza Joint
-      </h2>
+    <motion.div
+      className="home container"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1.5, delay: 1.5 }}
+    >
+      <h2>Welcome to Pizza Joint</h2>
       <Link to="/base">
-        <button>
+        <motion.button
+          whileHover={{
+            scale: 1.1,
+            textShadow: "0px 0px 8px rgb(255,255,255)",
+            boxShadow: "0px 0px 8px rgb(255,255,255)",
+          }}
+        >
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
--- src/components/Toppings.tsx
+++ src/components/Toppings.tsx
@@ -1,37 +1,54 @@
-import { Link } from 'react-router-dom';
-import { Pizza } from '../App';
+import { Link } from "react-router-dom";
+import { Pizza } from "../App";
+import { motion } from "framer-motion";
 
 interface Props {
-  addTopping: (topping: string) => void
+  addTopping: (topping: string) => void;
   pizza: Pizza;
 }
 
-const Toppings:React.FC<Props> = ({ addTopping, pizza }) => {
-  const toppings = ['mushrooms', 'peppers', 'onions', 'olives', 'extra cheese', 'tomatoes'];
+const Toppings: React.FC<Props> = ({ addTopping, pizza }) => {
+  const toppings = [
+    "mushrooms",
+    "peppers",
+    "onions",
+    "olives",
+    "extra cheese",
+    "tomatoes",
+  ];
 
   return (
     <div className="toppings container">
-      
       <h3>Step 2: Choose Toppings</h3>
       <ul>
-        {toppings.map(topping => {
-          const spanClass = pizza.toppings.includes(topping) ? 'active' : '';
+        {toppings.map((topping) => {
+          const spanClass = pizza.toppings.includes(topping) ? "active" : "";
           return (
-            <li key={topping} onClick={() => addTopping(topping)}>
-              <span className={spanClass}>{ topping }</span>
-            </li>
-          )
+            <motion.li
+              whileHover={{ scale: 1.3, color: "#f8e112", originX: 0 }}
+              transition={{ type: "spring", stiffness: 500 }}
+              key={topping}
+              onClick={() => addTopping(topping)}
+            >
+              <span className={spanClass}>{topping}</span>
+            </motion.li>
+          );
         })}
       </ul>
 
       <Link to="/order">
-        <button>
+        <motion.button
+          whileHover={{
+            scale: 1.1,
+            textShadow: "0px 0px 8px rgb(255,255,255)",
+            boxShadow: "0px 0px 8px rgb(255,255,255)",
+          }}
+        >
           Order
-        </button>
+        </motion.button>
       </Link>
-
     </div>
-  )
-}
+  );
+};
 
 export default Toppings;
