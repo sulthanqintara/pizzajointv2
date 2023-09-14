368328df3adaa94028c728967910757a2fb903d7
SulthanQintara <sulthanqintara@gmail.com>
2023-09-14 15:25:57 +0700

    Order Components Add animation orchestration

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -1,16 +1,45 @@
+import { Variants, motion } from "framer-motion";
 import { Pizza } from "../App";
 
 interface Props {
-  pizza:Pizza
+  pizza: Pizza;
 }
-const Order:React.FC<Props> = ({ pizza }) => {
+
+const containerVariants: Variants = {
+  hidden: { x: "100vw", opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      when: "beforeChildren",
+      mass: 0.4,
+      damping: 8,
+      staggerChildren: 0.4,
+    },
+  },
+};
+const childVariants: Variants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
+const Order: React.FC<Props> = ({ pizza }) => {
   return (
-    <div className="container order">
+    <motion.div
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      className="container order"
+    >
       <h2>Thank you for your order :)</h2>
-      <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
-    </div>
-  )
-}
+      <motion.p variants={childVariants}>You ordered a {pizza.base} pizza with:</motion.p>
+      <motion.div variants={childVariants}>
+        {pizza.toppings.map((topping) => (
+          <div key={topping}>{topping}</div>
+        ))}
+      </motion.div>
+    </motion.div>
+  );
+};
 
 export default Order;
